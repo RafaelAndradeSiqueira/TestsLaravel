b5b7f761477b75571b043a5f29467942109b9983
Rafael Andrade <r-andrade-siqueira@hotmail.com>
2026-03-10 10:53:37 -0300

    Login e logout testes

--- tests/Feature/loginTest.php
+++ tests/Feature/loginTest.php
@@ -4,9 +4,13 @@ namespace Tests\Feature;
 
 
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_if_user_can_access_route(): void
     {
         $response = $this->get('/login');
@@ -29,12 +33,13 @@ class LoginTest extends TestCase
 
     public function test_with_validate_erros(): void
     {
-        $response = $this->post('/login', [
+        $response = $this->from('/login')->post('/login', [
             'email' => '',
             'password' => '',
         ]);
 
         $response->assertStatus(302);
+        $response->assertRedirectBack('login');
     }
 
     public function test_if_password_or_email_are_incorrect(): void
@@ -47,5 +52,41 @@ class LoginTest extends TestCase
         $response->assertStatus(302);
         $response->assertSessionHasErrors(['email', 'password']);
         $response->assertRedirect('/login');
+    }
+
+    public function test_if_user_cant_page_login_authenticated(): void
+    {
+
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/');   
+    }
+
+    public function test_if_user_can_login(): void
+    {
+        User::factory()->create([
+            'email' => 'teste@gmail.com',
+            'password' => bcrypt('password'),
+       ]);
+
+        $response = $this->post('/login', [
+                'email' => 'teste@gmail.com',
+                'password' => 'password',
+            ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/');
+    }
+
+    public function test_if_user_can_logout(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->post('/logout');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/');
     }
 }
